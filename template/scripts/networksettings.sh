16dc9d98263ece6dafe8cb5b10a0f4f89bd5a44c
Johannes Fürmann <waaaaargh@quantentunnel.de>
2013-06-21 03:46:17 +0200

    moves to internal networking (was: host-only)

--- template/scripts/networksettings.sh
+++ template/scripts/networksettings.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+ifconfig eth1 up
+ifconfig eth1 $1
 ## delete all existing default routes
 while ip route del default; do :; done
 
+
 ## add new default royte
-ip route add 0.0.0.0/0 via $1
-echo nameserver $2 > /etc/resolv.conf
+ip route add 0.0.0.0/0 via $2
+echo nameserver $3 > /etc/resolv.conf
--- template/scripts/router_iptables.sh
+++ template/scripts/router_iptables.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-## delete iptables routes
-/sbin/iptables -F
-/sbin/iptables -X
-/sbin/iptables -t nat -F
-
 ## enable packet forwarding
 /sbin/sysctl -w net.ipv4.ip_forward=1
 
+## delete iptables routes
+/sbin/iptables -F
+/sbin/iptables --delete-chain
+/sbin/iptables -t nat -F
+/sbin/iptables -t nat --delete-chain
+
 ## set interface forwarding
 iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
+
+## set interface accept
 iptables --append FORWARD --in-interface eth1 -j ACCEPT
 iptables --append FORWARD --in-interface eth2 -j ACCEPT
