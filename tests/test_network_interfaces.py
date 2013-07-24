5bfb0f7826ae56ae28e212fe8eea98845733d89f
Johannes Fürmann <waaaaargh@quantentunnel.de>
2013-07-24 17:27:25 +0200

    port network_interface_test to unittest.

--- tests/test_network_interfaces.py
+++ tests/test_network_interfaces.py
@@ -1,15 +1,15 @@
-import sys
-sys.path.append('..')
+from unittest import TestCase
 
-def test_internal_networking_interface():
-    from evilgenius.vagrant import VagrantBox, VBoxInternalNetworkingInterface
+class TestNetworkingInterfaces(TestCase):
+    def test_internal_networking_interface(self):
+        from evilgenius.vagrant import VagrantBox, VBoxInternalNetworkingInterface
 
-    testbox = VagrantBox(box="precise32", install_scripts=["echo 'Hello, World!'"],
-                         name='testbox')
+        testbox = VagrantBox(box="precise32", install_scripts=["echo 'Hello, World!'"],
+                            name='testbox')
 
-    iface = VBoxInternalNetworkingInterface(address="10.11.12.13", network_name="asdf")
+        iface = VBoxInternalNetworkingInterface(address="10.11.12.13", network_name="asdf")
 
-    testbox.network_interfaces += [iface]
+        testbox.network_interfaces += [iface]
 
-    assert "nic" in testbox.definition
-    assert "asdf" in testbox.definition
+        self.assertTrue("nic" in testbox.definition)
+        self.assertTrue("asdf" in testbox.definition)
