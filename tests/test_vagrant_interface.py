1ad64792420d82136c0904825882f4ab0c91b582
Johannes Fürmann <waaaaargh@quantentunnel.de>
2013-07-25 15:33:14 +0200

    rewrite some of the vagrantInterface tests

--- tests/test_vagrant_interface.py
+++ tests/test_vagrant_interface.py
@@ -1,19 +1,36 @@
-import sys
-sys.path.append('..')
+from unittest import TestCase
+import os
 
-def test_vagrant_status():
-    from evilgenius.vagrant import VagrantController
 
-    import tempfile
+class TestVagrantInterface(TestCase):
+    def test_vagrant_init(self):
+        from evilgenius.vagrant import VagrantController
+        import tempfile
 
-    testdir = tempfile.mkdtemp()
+        tempdir = tempfile.mkdtemp()
 
-    c = VagrantController(root=testdir)
-    c.init()
-    assert len(c.status()) > 0
-    c.up()
-    assert c.status()['default'] == 'running'
-    a = c.run_command('echo "Hello, World!"')
-    assert a == ['Hello, World!\n']
-    c.destroy()
-    assert c.status()['default'] == 'not created'
+        cont = VagrantController(root=tempdir)
+
+        cont.init()
+
+        with open(os.path.join(tempdir, 'Vagrantfile')) as f:
+            self.assertGreater(len(f.read()), 0)
+
+    def test_vagrant_status(self):
+        from evilgenius.vagrant import VagrantController
+        import tempfile
+
+        tmpdir = tempfile.mkdtemp()
+        cont = VagrantController(root=tmpdir)
+
+        def mock_status_output(command):
+            return (0, ['Current machine states:\n', '\n',
+                        'default                  not created (virtualbox)\n',
+                        '\n', 'The environment has not yet been created. Run `vagrant up` to\n',
+                        'create the environment. If a machine is not created, only the\n',
+                        'default provider will be shown. So if a provider is not listed,\n',
+                        'then the machine is not created for that environment.\n'])
+        cont._vagrant = mock_status_output
+
+        self.assertEqual(cont.status(), {'default': 'not created'})
+        self.assertEqual(cont.status(vm='default'), 'not created')
