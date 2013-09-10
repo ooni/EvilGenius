d8e33325221aaa2c77e056d4fbcf341d2e409996
Johannes Fürmann <johannes@weltraumpflege.org>
2013-09-10 13:48:11 +0200

    fix broken test

--- tests/test_network_interfaces.py
+++ tests/test_network_interfaces.py
@@ -4,7 +4,7 @@ class TestNetworkingInterfaces(TestCase):
     def test_internal_networking_interface(self):
         from evilgenius.vagrant import VagrantBox, VBoxInternalNetworkingInterface
 
-        testbox = VagrantBox(box="precise32", install_scripts=["echo 'Hello, World!'"],
+        testbox = VagrantBox(box="precise32", install=["echo 'Hello, World!'"],
                             name='testbox')
 
         iface = VBoxInternalNetworkingInterface(address="10.11.12.13", peer_address="10.11.12.14", network_name="asdf")
